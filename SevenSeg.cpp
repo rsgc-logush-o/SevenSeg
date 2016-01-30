15f9d7134dbe956ce397a4d4adc5ae6b50b5c3c6
rsgc-logush-o <ologush@rsgc.on.ca>
2016-01-30 09:03:29 -0500

    Started on floating point
    I have started working on floating point displaying.

--- SevenSeg.cpp
+++ SevenSeg.cpp
@@ -17,7 +17,7 @@ SevenSeg::SevenSeg(uint8_t numberOfDisplays, uint8_t clockPin, uint8_t dataPin, 
 	numbers[] = {B11100111, B00100001, B11001011, B01101011, B00101101, B01101110, B11101110, B00100011, B11101111, B01101111};
 }
 
-void SevenSeg::display(uint8_t numberToDisplay)
+void SevenSeg::displayInt(uint8_t numberToDisplay)
 {
 	if(numberToDisplay >= pow(10, _numberOfDisplays + 1))
 	{
@@ -27,23 +27,25 @@ void SevenSeg::display(uint8_t numberToDisplay)
 
 		digitalWrite(_latchPin, HIGH);
 	}
-
-	int numbersToShow[numberOfDisplays];
-
-	for(int i = _numberOfDisplays - 1; i > -1; i--)
+	else
 	{
-		numbersToShow[i] = numberToDisplay / pow(10, i);
-		numberToDisplay -= numbersToShow[i] * pow(10, i);
+		int numbersToShow[numberOfDisplays];
+
+		for(int i = _numberOfDisplays - 1; i > -1; i--)
+		{
+			numbersToShow[i] = numberToDisplay / pow(10, i);
+			numberToDisplay -= numbersToShow[i] * pow(10, i);
+		}
+
+		digitalWrite(_latchPin, LOW);
+
+		for(int i = _numberOfDisplays - 1; i > -1; i--)
+		{
+			shiftData(numbersToShow[i]);
+		}
+
+		digitalWrite(_latchPin, HIGH);
 	}
-
-	digitalWrite(_latchPin, LOW);
-
-	for(int i = _numberOfDisplays - 1; i > -1; i--)
-	{
-		shiftData(numbersToShow[i]);
-	}
-
-	digitalWrite(_latchPin, HIGH);
 }
 
 void SevenSeg::shiftData(uint8_t dataToShift)
@@ -57,4 +59,9 @@ void SevenSeg::shiftData(uint8_t dataToShift)
 		digitalWrite(_clockPin, HIGH);
 
 	}
+}
+
+void SevenSeg::displayFloat(float numberToDisplay, uint8_t floatWidth)
+{
+
 }
--- sevenSeg.h
+++ sevenSeg.h
@@ -1,3 +1,7 @@
+/* A seven segment display library for the 74HC595 shift registers
+Written By Oliver Logush in 2016, Version 1*/
+
+
 #ifndef SevenSeg_h
 #define SevenSeg_h
 
@@ -7,7 +11,8 @@ class SevenSeg
 {
   public:
     SevenSeg(uint8_t numberOfDisplays, uint8_t clockPin, uint8_t dataPin, uint8_t latchPin);
-    void display(uint8_t displayNumber, uint8_t numberToDisplay);
+    void displayInt(uint8_t numberToDisplay);
+    void displayFloat(float numberToDisplay, uint8_t floatWidth);
   private:
     uint8_t _numberOfDisplays;
     uint8_t _clockPin;
